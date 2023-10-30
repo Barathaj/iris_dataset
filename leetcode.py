11a73951783b592062b50ae6b5bb2a6671e9a1ce
Barath Raj <130913642+Barathaj@users.noreply.github.com>
2023-10-31 00:17:45 +0530

    Add files via upload

---
+++ leetcode.py
@@ -0,0 +1,46 @@
+import streamlit as st
+import numpy as np
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn import datasets
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+
+iris=datasets.load_iris()
+x=iris.data
+y=iris.target
+
+x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.3,random_state=42)
+
+# Assuming you have x_train and y_train loaded from your dataset
+
+# Create and fit the KNeighborsClassifier
+k_near = KNeighborsClassifier(n_neighbors=3)
+k_near.fit(x_train, y_train)
+
+# Streamlit app
+st.title("Iris Flower Prediction App")
+
+# Sidebar with input fields
+st.sidebar.header("Input Parameters")
+
+# Input fields for Sepal Length, Sepal Width, Petal Length, and Petal Width
+sepal_length = st.sidebar.slider("Sepal Length", 4.0, 8.0, 5.0)
+sepal_width = st.sidebar.slider("Sepal Width", 2.0, 4.5, 3.5)
+petal_length = st.sidebar.slider("Petal Length", 1.0, 7.0, 4.0)
+petal_width = st.sidebar.slider("Petal Width", 0.1, 2.5, 1.0)
+
+# User input data
+user_input = np.array([[sepal_length, sepal_width, petal_length, petal_width]])
+
+# Make prediction
+prediction = k_near.predict(user_input)
+
+# Display the predicted class
+result=prediction[0]
+if result==0:
+    st.title("solosa")
+elif result==1:
+    st.title("versicolor")
+else:
+    st.title("virginica")
